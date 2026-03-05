97ca9db8e18cd5c9b36446efdd322c91480474ee
AnnTishaeva05 <tihaeva05@gmail.com>
2026-03-05 10:21:11 +0700

    Add .gitignore for ProfileProject

--- backend/ProfileProject/obj/Debug/net8.0/ProfileProject.AssemblyInfo.cs
+++ backend/ProfileProject/obj/Debug/net8.0/ProfileProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProfileProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+749b382602b63628d13f7488298e1ba46283b908")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProfileProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProfileProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
